2f5d37f548e58a07edbe6988db870ea763626882
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-06-03 20:57:50 +0330

    working on Detail component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import { CommentCard } from "@/components/Comment/CommentCard/CommentCard";
+import {Detail} from "@/components/Blog/Detail/Detail"
+
 
 export default function Home() {
     return (
       <section className="pt-[72px] pb-96 md:pt-[104px]">
         <div className="text-justiify pt-20">
-        <CommentCard
-          userName="مـــحمدرضا سلطانـــــــی"
-          degree="دانش‌آموز هفتم مدرسۀ شهریار"
-          rating={4.5}
-          date="1403/03/10"
-          comment="این مدرسه جزو بهترین مدارس ایران هست، از وقتی ایـنجا ثبت‌نام کـردم عــملکردم خیلی بـهتر شده
-و خیلی رسیدگی خوبی دارن، کلی دوست جدید پیــــدا کردم و مدرسه کلی بــــرنامه‌های خفن و جـــذاب
-داره واسمون، راستی اردو هم اینجا زیاد داریم."
-          avatarUrl="https://i.pravatar.cc/150?img=32"
-        />
+        <Detail
+        avatarUrl="https://i.pravatar.cc/150?img=32"
+        teller="دکتر احمدی"
+        releaseDate="۱۴۰۴/۰۳/۱۲"
+        duration="۷ دقیقه"
+        category="یادگیری معکوس"
+      />
         </div>
       </section>
     );
--- components/Blog/Detail/Detail.tsx
+++ components/Blog/Detail/Detail.tsx
@@ -1,17 +1,15 @@
 import * as React from "react";
 import { Avatar } from "@heroui/avatar";
 import { Icon } from '@iconify/react';
-import {
-    Breadcrumbs,
-    BreadcrumbItem
-  } from "@heroui/breadcrumbs"
-import { Button } from "@heroui/react";
+import {Breadcrumbs, BreadcrumbItem} from "@heroui/breadcrumbs";
+import { Button } from "@heroui/button";
+import Link from "next/link";
 
 
 
 type DetailTypeProps = {
   avatarUrl?: string;
-  teller: string;
+  teller: string | React.ReactNode;
   releaseDate: string;
   duration: string | number;
   category: string;
@@ -24,40 +22,62 @@ const InfoItem: React.FC<{ icon: string; label: string }> = ({ icon, label }) =>
   </div>
 );
 
-export const CommentCard: React.FC<DetailTypeProps> = ({
+export const Detail: React.FC<DetailTypeProps> = ({
   avatarUrl,
   category,
   duration,
   releaseDate,
   teller
 }) => {
+
+
+    const breadCrumbs = [
+        {
+            title: "مقالات",
+            url: "/hybrid"
+        },
+        {
+            title: "آموزشی",
+            url: "/hybrid"
+        },
+        {
+            title: "آموزش معکوس و تاثیرات آن بر یادگیری",
+            url: "/hybrid"
+        },
+    ]
+
   return (
     <div className="w-full flex justify-between items-center gap-3">
       <div className="flex flex-row justify-center items-center gap-4">
-        <Avatar src={avatarUrl} size="lg" />
+        <Avatar src={avatarUrl} size="md" />
 
-        <InfoItem icon="fluent:calendar-date-20-regular" label={`از زبان: ${teller}`} />
+        <InfoItem icon="fluent:person-circle-28-filled" label={`از زبان: ${teller}`} />
         <InfoItem icon="fluent:calendar-date-20-regular" label={`انتشار: ${releaseDate}`} />
-        <InfoItem icon="fluent:calendar-date-20-regular" label={`زمان مطالعه: ${duration}`} />
+        <InfoItem icon="fluent:clock-alarm-20-regular" label={`زمان مطالعه: ${duration}`} />
         <InfoItem icon="fluent:calendar-date-20-regular" label={`دسته‌بندی: ${category}`} />
       </div>
 
       <div className="flex flex-row items-center">
-      <Breadcrumbs
-      itemClasses={{
-        item: "px-2",
-        separator: "/",
-      }}
-    >
-      <BreadcrumbItem href="#">مقالات</BreadcrumbItem>
-      <BreadcrumbItem href="#">آموزشی</BreadcrumbItem>
-      <BreadcrumbItem href="#">آموزش معکوس و تاثیرات آن بر یادگیری</BreadcrumbItem>
-      </Breadcrumbs>
+        <Breadcrumbs
+            underline="hover"
+            color="secondary"
+            className="ps-12  w-full "
+            classNames={{base: "inline-grid", list: "flex-nowrap w-full overflow-y-hidden pb-2"}}
+            separator="/"
+            >
+                {breadCrumbs.map(({title, url}, idx) => (
+                    <BreadcrumbItem key={idx} href={url}>
+                        {title}
+                     </BreadcrumbItem>
+                        ))}
+        </Breadcrumbs>
+      
 
       <Button
+      size="lg"
         isIconOnly
         startContent={<Icon icon="fluent:arrow-left-16-regular" width="24" height="24" />}
-        className="px-2 py-4 bg-foreground/10" 
+        className="px-3 py-5 bg-foreground/10" 
       />
       </div>
     </div>
