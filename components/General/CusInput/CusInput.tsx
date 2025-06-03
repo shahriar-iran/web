fc244cd77bba33e79507c8241ee7afe919508709
Saleh Moghadam <salehmoghadam30@gmail.com>
2025-06-03 21:33:19 +0330

    working CusInput & CusSelect

---
+++ components/General/CusInput/CusInput.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+
+
+
+type CusInputTypeProps = {
+
+}
+
+
+export const CusInput: React.FC<CusInputTypeProps> = (props) => {
+    const {
+
+    } = props;
+    return(
+        <></>
+    )
+}
---
+++ components/General/CusSelect/CusSelect.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+
+
+
+type CusSelectTypeProps = {
+
+}
+
+
+export const CusSelect: React.FC<CusSelectTypeProps> = (props) => {
+    const {
+
+    } = props;
+    
+    
+    return(
+        <></>
+ )
+}
