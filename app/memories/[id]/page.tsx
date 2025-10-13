5ac31cf4464fb39e2e54affc7fec88964df7e41a
rasoul707 <r.ahmadifar.1377@gmail.com>
2025-10-13 18:55:24 +0330

    fix error

--- app/memories/[id]/page.tsx
+++ app/memories/[id]/page.tsx
@@ -45,13 +45,13 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
         {/* نمایش خاطره انتخاب شده */}
         <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-2 2xl:px-0 mx-auto">
             <div className="">
-                <VideoPlayer/>
+                <VideoPlayer src={""}/>
             </div>
             <div className="">
-                <VideoPlayer/>
+                <VideoPlayer src={""}/>
             </div>
             <div className="">
-                <VideoPlayer/>
+                <VideoPlayer src={""}/>
             </div>
         </div>
         <IntroVideoSection/>
